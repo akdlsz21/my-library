e55fb8e877f3c6f7c53c05f49fb70679db7f3f94
jihoon <akdlsz21@gmail.com>
2021-06-22 16:16:14 +0900

    when submit, existing nodes in list will be removed

--- script.js
+++ script.js
@@ -22,21 +22,8 @@ let pages = document.querySelector('.pages');
 let read = document.querySelector('.read');
 let submit = document.querySelector('.submit');
 let form = document.querySelector('.form');
-
-// function to add book object to mylibrary array and print info to list
-function addBookToLibrary(title, author, pages, read) {
-	const book = new Book(title, author, pages, read);
-	myLibrary.push(book);
-	printBookInfo(myLibrary);
-}
-
-// function to textContent myLibrary array text
-function printBookInfo(array) {
-	array.forEach((element) => {
-		// needs to create new li element to attach element.info();//
-		return (bookList.textContent = element.info());
-	});
-}
+let bookListChildNode = bookList.childNodes;
+// let list = document.querySelectorAll('.list');
 
 // function when submit, elements in form will push into myLibrary.
 form.addEventListener('submit', (e) => {
@@ -52,3 +39,30 @@ form.addEventListener('submit', (e) => {
 
 	addBookToLibrary(titleArg, authorArg, pagesArg, readArg);
 });
+
+// function to add book object to mylibrary array and print info to list
+function addBookToLibrary(title, author, pages, read) {
+	const book = new Book(title, author, pages, read);
+	myLibrary.push(book);
+	printBookInfo(myLibrary);
+}
+
+// function to textContent myLibrary array text
+function printBookInfo(array) {
+	eraseListChild();
+	array.forEach((element) => {
+		// needs to create new li element to attach element.info();//
+		let newList = document.createElement('li');
+		newList.className = 'list';
+		let listTextNode = document.createTextNode(element.info());
+		newList.textContent = listTextNode.textContent;
+		bookList.appendChild(newList);
+	});
+}
+
+// function to erase childElementNode in list,
+function eraseListChild() {
+	while (bookList.lastChild) {
+		bookList.removeChild(bookList.lastChild);
+	}
+}
